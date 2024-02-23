74d74b8e54436ea0521b49a6abb99d563e59c567
dvidCR <dvidcr100@gmail.com>
2024-02-23 14:50:19 +0100

    Comenzando el ranking

--- JS/ranking.js
+++ JS/ranking.js
@@ -1,3 +1,17 @@
 import { getRanking } from "./utils.js";
 
-console.log(getRanking())
+let position = [];
+var ranking = getRanking();
+// ['AlppZCuZL4CXReBgSWHI', 'Usuario1', 0]
+
+for (var i = 0; i < ranking.length - 1; i++) {
+    if (position[i] != position[i + 1]) {
+        if (ranking[i][2] < ranking[i + 1][2]) {
+            position.push(ranking[i + 1][1]);
+        } else {
+            position.push(ranking[i][1]);
+        }
+    }
+}
+
+console.log(position)
