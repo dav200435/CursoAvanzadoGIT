9fc90cac1f9e7186dfbea70f8cc2641429c8b91d
dvidCR <dvidcr100@gmail.com>
2024-02-24 14:24:18 +0100

    ranking.js terminado

--- JS/ranking.js
+++ JS/ranking.js
@@ -13,18 +13,30 @@ function repeat(r) {
     return true;
 }
 
+function rerun() {
+    for (var i = 0; i < ranking.length; i++) {
+        if (repeat(ranking[i][1])) {
+            position.push([ranking[i][1], ranking[i][2]]);
+        }
+    }
+}
+
 for (var i = 0; i < ranking.length; i++) {
-    var aux = ["", ""];
-    var aux2 = ["", ""]
+    let aux = ["",""];
     for (var j = 0; j < ranking.length; j++) {
         if (repeat(ranking[j][1])) {
             aux[1] < ranking[j][2] ? aux = [ranking[j][1], ranking[j][2]]: aux = aux;
         }
     }
 
-    if (repeat(aux[0])) {
+    if (repeat(aux[0]) && aux[0] !== "") {
         position.push(aux);
     }
 }
+rerun();
 
-console.log(position)
+
+for (var i = 0; i < 5; i++){
+    var rank = document.getElementById(`pos${i + 1}`);
+    rank.innerHTML = `<p><strong>${position[i][0]}<br>${position[i][1]}</storng></p>`;
+}
